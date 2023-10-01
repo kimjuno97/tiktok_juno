2ff303481b77602f1e45532ddf80aa58f645acd5
rogan <jh241093@gmail.com>
2023-10-01 13:05:41 +0900

    AppBar style은 main.dart에서 정의할 수 있음 그리고 내부에만 쓰이는 변수는 '_'으로 private로 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_juno/features/authentication/sign_up_screen.dart';
 
+import 'constants/sizes.dart';
+
 void main() {
   runApp(const TikTokApp());
 }
@@ -12,6 +14,18 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok Clone',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
         primaryColor: const Color(0xFFE9435A),
       ),
       home: const SignUpScreen(),
