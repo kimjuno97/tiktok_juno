2030e30f80da4b380c5df0e22c545e46a60d7367
rogan <jh241093@gmail.com>
2023-10-15 17:35:41 +0900

    SettingsScreen 구현

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,6 +10,112 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
+
+  /// dataPicker 3가지 종류
+  Future<void> _datePickers() async {
+    final date = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1980),
+      lastDate: DateTime(2030),
+    );
+    print(date);
+    if (!mounted) return;
+    final time = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    print(time);
+    if (!mounted) return;
+    final booking = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(1980),
+      lastDate: DateTime(2030),
+      builder: (context, child) {
+        return Theme(
+          data: ThemeData(
+              appBarTheme: const AppBarTheme(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.black)),
+          child: child!,
+        );
+      },
+    );
+    print(booking);
+  }
+
+  void _iosLogout() {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: const Text("Are you sure?"),
+        content: const Text("Plx dont go"),
+        actions: [
+          CupertinoDialogAction(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("No"),
+          ),
+          CupertinoDialogAction(
+            onPressed: () => Navigator.of(context).pop(),
+            isDestructiveAction: true,
+            child: const Text("Yes"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _androidLogout() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        icon: const FaIcon(FontAwesomeIcons.skull),
+        title: const Text("Are you sure?"),
+        content: const Text("Plx dont go"),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const FaIcon(FontAwesomeIcons.car),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Yes"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _iosPopupLogout() {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (context) => CupertinoActionSheet(
+        title: const Text("Are you sure?"),
+        message: const Text("Please dooooont gooooo"),
+        actions: [
+          CupertinoActionSheetAction(
+            isDefaultAction: true,
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Not log out"),
+          ),
+          CupertinoActionSheetAction(
+            isDestructiveAction: true,
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Yes plz."),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,21 +124,43 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
       body: ListView(
         children: [
-          ListTile(
-            onTap: () => showAboutDialog(
-                context: context,
-                applicationVersion: "1.0",
-                applicationLegalese:
-                    "All rights reseverd. Please dont copy me."),
-            title: const Text(
-              "About",
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: const Text("About this app....."),
+          SwitchListTile.adaptive(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+            subtitle: const Text("They will be cute."),
           ),
-          const AboutListTile(),
+          CheckboxListTile(
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Marketing emails"),
+            subtitle: const Text("We won't spam you."),
+          ),
+          ListTile(
+            onTap: _datePickers,
+            title: const Text("What is your birthday?"),
+            subtitle: const Text("I need to know!"),
+          ),
+          ListTile(
+            title: const Text("Log out (iOS)"),
+            textColor: Colors.red,
+            onTap: _iosLogout,
+          ),
+          ListTile(
+            title: const Text("Log out (Android)"),
+            textColor: Colors.red,
+            onTap: _androidLogout,
+          ),
+          ListTile(
+            title: const Text("Log out (iOS / Bottom)"),
+            textColor: Colors.red,
+            onTap: _iosPopupLogout,
+          ),
+          const AboutListTile(
+            applicationVersion: "1.0",
+            applicationLegalese: "Don't copy me.",
+          ),
         ],
       ),
     );
