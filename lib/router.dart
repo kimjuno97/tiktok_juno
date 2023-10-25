fc380120e24035ce3e66f2f5001bc671fab6e9b7
rogan <jh241093@gmail.com>
2023-10-25 19:37:32 +0900

    permission_handler로 권한 핸들링하고 camera세팅

--- lib/router.dart
+++ lib/router.dart
@@ -1,42 +1,12 @@
 import 'package:go_router/go_router.dart';
 
-import 'features/authentication/email_screen.dart';
-import 'features/authentication/login_screen.dart';
-import 'features/authentication/sign_up_screen.dart';
-import 'features/authentication/username_screen.dart';
-import 'features/users/user_profile_screen.dart';
+import 'features/videos/vide_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => const SignUpScreen(),
-    ),
-    GoRoute(
-      name: LoginScreen.routeName,
-      path: LoginScreen.routeURL,
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      name: UsernameScreen.routeName,
-      path: UsernameScreen.routeURL,
-      builder: (context, state) => const UsernameScreen(),
-    ),
-    GoRoute(
-      name: EmailScreen.routeName,
-      path: EmailScreen.routeURL,
-      builder: (context, state) {
-        final args = state.extra as EmailScreenArgs;
-        return EmailScreen(username: args.username);
-      },
-    ),
-    GoRoute(
-      path: "/users/:username",
-      builder: (context, state) {
-        final username = state.pathParameters['username'];
-        return UserProfileScreen(username: username!, tab: '');
-      },
+      path: "/",
+      builder: (context, state) => const VideoRecordingScreen(),
     )
   ],
 );
